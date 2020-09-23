5ec2024877ee71032dcf8420a256725b4083fef2
MatriMariem <meriemmatri1994@gmail.com>
2020-09-23 12:53:11 +0100

    last mandatory task

---
+++ 0x0C-Javascript_advanced/14-wikipedia.js
@@ -0,0 +1,14 @@
+function createElement (data) {
+  p = document.createElement('p');
+  p.innerHTML = JSON.stringify(data);
+  document.body.appendChild(p);
+}
+function queryWikipedia (callback) {
+  let x = new XMLHttpRequest();
+  x.open('GET', 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*', true);
+  x.send();
+  x.onload = function () {
+    callback(JSON.parse(x.response)['extract']);
+  }
+}
+queryWikipedia(createElement);
