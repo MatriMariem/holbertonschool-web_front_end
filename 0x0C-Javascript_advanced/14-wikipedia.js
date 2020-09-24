4a2e02e9a10eaf7e0507d13ddf0099a4fa6e7c04
MatriMariem <meriemmatri1994@gmail.com>
2020-09-24 22:31:57 +0100

    finish javascript project

--- 0x0C-Javascript_advanced/14-wikipedia.js
+++ 0x0C-Javascript_advanced/14-wikipedia.js
@@ -7,8 +7,10 @@ function queryWikipedia (callback) {
   let x = new XMLHttpRequest();
   x.open('GET', 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*', true);
   x.send();
-  x.onload = function () {
-    callback(JSON.parse(x.response)['extract']);
+  x.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+     createElement(JSON.parse(x.response).query.pages["21721040"].extract);
+   }
   }
 }
 queryWikipedia(createElement);
